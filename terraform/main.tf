f9f2095e3c3ac5e72052f249a623f75e05407a41
James Kehinde <kennyjames104@gmail.com>
2026-03-24 13:01:55 -0700

    Fix eks module argument compatibility for terraform-aws-modules/eks v21: use vpc_id and subnet_ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 data "aws_ami" "amazon_linux2" {
   most_recent = true
   owners      = ["amazon"]
@@ -26,11 +37,8 @@ module "eks" {
   name               = var.eks_cluster_name
   kubernetes_version = var.eks_version
 
-  create_vpc = true
-  vpc_cidr   = "10.0.0.0/16"
-  azs        = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
 
   eks_managed_node_groups = {
     on_demand = {
